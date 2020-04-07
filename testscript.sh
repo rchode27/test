5b5515063b1c0fe450bd008af7192cf71da3106c
rchode27 <rchode27@gmail.com>
2020-04-07 21:57:24 +0000

    my first commit

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #echo "this is an example of a script"
+#This is my first script
 echo "this is running in a build step"
 echo "this is a param "$PASSED""
 num=$(($FIRSTNUMBER + $SECONDNUMBER))
